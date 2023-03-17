1ca30090b6851b1e122d06f4e6335f3ad9ef8c3b
Adam R <adamrobinson55@gmail.com>
2023-03-16 21:34:19 -0500

    deleted main folder from file to avoid looking at it

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,26 @@
 // Assignment Code
+
+function generatePassword() {
+  var length = parseInt(
+    prompt("Yo Dawg, How many charactizzles you want up is hizzle")
+  );
+
+  if(Number.isNaN(length)) {
+    alert("Hey What the flip?? that's whack enter a number, i'm returning NULL punk")
+    return null
+  }
+
+  if (length<8) {
+    alert("ooooo, sorry that's a little too small...")
+    alert("try a number that's 8 or larger :^)")
+    return null
+  }
+
+  return "nice"
+
+}
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
