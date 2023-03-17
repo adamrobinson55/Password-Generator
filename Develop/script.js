163b54736ae86be1fb0eb618aaa8c319a1f366b9
Adam R <adamrobinson55@gmail.com>
2023-03-17 13:34:59 -0500

    added a musCon (must Contain) array that adds at least of one each required thing to the password, I actually think that it's done? I need to add a README file I guess...

--- Develop/script.js
+++ Develop/script.js
@@ -5,27 +5,22 @@
 // I've got 19 symbols here
 var sym = ["!", "@", "#","$", "%", "^", "&", "*", "(", ")", ",", "." ,"?" ,";" ,":", "-", "_", "+", "="]
 
-// all lowercase letters I can use the uhhhhhhh toUpperCase() method to convert them to Upper case if I need to
+// all lowercase letters I can use the toUpperCase() method to convert them to Upper case if I need to
 // there are 26
 var let = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
 
-// all numbers there are 10 possibilities for numbers, I also don't think I need to make this becuase math.Floor(math.Rand() * 10) would return these but whatever 
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
 //variables
+var musCon = []
+var musConLen
 var eRand
 var boolU
 var boolL
 var boolS
 var boolN
+var symLen = sym.length
 
 var length
-var pass =[]
-// this little for loop works for putting all possible letters into the console, which is kind of pog
-// for(let i = 0; i < 50; i++){
-// console.log(let[Math.floor(Math.random() * 26)])
-// console.log(let[Math.floor(Math.random() * 26)].toUpperCase())
-// }
+var pass = []
 var i
 function genRandy(i) {
   return Math.floor(Math.random() * i)
@@ -33,6 +28,8 @@ function genRandy(i) {
 
 
 function generatePassword() {
+
+
   var length = parseInt(
     prompt("How many characters would you like in your password?")
   );
@@ -60,27 +57,48 @@ function generatePassword() {
   var boolS = confirm("Should your password contain Symbols?");
 
   var boolN = confirm("Should your password contain Numbers?");
-  
-  for(let i = 0; i < length;) {
+
+  if (boolU) {
+    musCon.push(let[genRandy(26)].toUpperCase())
+  }
+  if (boolL) {
+    musCon.push(let[genRandy(26)])
+  }
+  if (boolS) {
+    musCon.push(sym[genRandy(symLen)])
+  }
+  if (boolN) {
+    musCon.push(genRandy(10))
+  }
+
+  console.log(musCon) //remove this later
+
+  musConLen = musCon.length
+
+  for(let i = musConLen; i < length;) {
     eRand = genRandy(4)
 
     if (eRand == 0 && boolN == true){
       pass.push(genRandy(10))
       i++}
+
     if (eRand == 1 && boolL == true)  {
       pass.push(let[genRandy(26)])
       i++}
+
     if (eRand == 2 && boolU == true)  {
       pass.push(let[genRandy(26)].toUpperCase())
       i++}
+
     if (eRand == 3 && boolS == true) {
-      pass.push(sym[genRandy(19)])
+      pass.push(sym[genRandy(symLen)])
       i++}
   }
-
+  pass = pass.concat(musCon)
   return pass.join("")
 }
 //this works pretty well but it's possible that you could exclude required contents because it's completely random
+//making a primary array that  contains all the manditory things that way passwords always contain one of each thing
 
 var generateBtn = document.querySelector("#generate");
 
