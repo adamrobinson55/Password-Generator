c2d19edd9fed7b992fa7112f2a98c8238572d015
Adam R <adamrobinson55@gmail.com>
2023-03-22 11:02:14 -0500

    fixed a bug where you couldn't create multiple passwords without refreshing the page

--- Develop/script.js
+++ Develop/script.js
@@ -18,39 +18,40 @@ var boolL
 var boolS
 var boolN
 var symLen = sym.length
-
 var length
 var pass = []
 var i
+
 function genRandy(i) {
   return Math.floor(Math.random() * i)
 }
 
 
 function generatePassword() {
-
+  pass = []
 
   var length = parseInt(
-    prompt("How many characters would you like in your password?")
+    prompt("How many characters would you like in your password?") //gets the number of characters and saves it to a variable
   );
 
   if(Number.isNaN(length)) {
-    alert("Please enter a number:")
+    alert("Please enter a number") // if length is not saved as a number this ends the function
     return null
   };
 
   if (length<8) {
-    alert("Your password must be at least 8 characters long.")
+    alert("Your password must be at least 8 characters long.") // if length is too short this ends the function and returns null
     alert("Enter a number that is 8 or greater.")
     return null
   };
 
   if (length>128) {
-    alert("Your password cannot be longer than 128 characters.")
+    alert("Your password cannot be longer than 128 characters.") // if length is too long this ends the function and returns null
     alert("Enter a number that is 128 or less.")
+    return null
   };
 
-  var boolU = confirm("Should your password contain Uppercase Letters?");
+  var boolU = confirm("Should your password contain Uppercase Letters?"); // lines 53-59 save booleans to ask about uppercase, lowercase, symbols and numbers
 
   var boolL = confirm("Should your password contain Lowercase Letters?");
 
@@ -58,25 +59,26 @@ function generatePassword() {
 
   var boolN = confirm("Should your password contain Numbers?");
 
-  if (boolU) {
+  if (boolU) { //lines 61-72 create a 1-4 long array that contains at least of each required character type.
     musCon.push(let[genRandy(26)].toUpperCase())
-  }
+  } 
   if (boolL) {
     musCon.push(let[genRandy(26)])
   }
   if (boolS) {
     musCon.push(sym[genRandy(symLen)])
-  }
+  } 
   if (boolN) {
     musCon.push(genRandy(10))
   }
-
-  console.log(musCon) //remove this later
-
+  if (boolU === false && boolL === false && boolS === false && boolN === false) { // this checks if the user selected no for each of the confirms on 53-59, sends an alert and returns null
+    alert("Your password must contain at least one type of character")
+    return null
+  }
   musConLen = musCon.length
 
-  for(let i = musConLen; i < length;) {
-    eRand = genRandy(4)
+  for(let i = musConLen; i < length;) { // i is set to musConLen so that when musCon is added to the end the password is the correct length
+    eRand = genRandy(4) // random number to select a random character type, if the random number selects a unselected character type the for loop resets without incrementing.
 
     if (eRand == 0 && boolN == true){
       pass.push(genRandy(10))
@@ -94,11 +96,9 @@ function generatePassword() {
       pass.push(sym[genRandy(symLen)])
       i++}
   }
-  pass = pass.concat(musCon)
-  return pass.join("")
+  pass = pass.concat(musCon) // ataches the musCon and password arrays
+  return pass.join("")  // joining with an empty string turns an array into a string with no commas seperating elements
 }
-//this works pretty well but it's possible that you could exclude required contents because it's completely random
-//making a primary array that  contains all the manditory things that way passwords always contain one of each thing
 
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# week-three-challenge
-create a password generator
+# Password Generator
+
+## What it does
+
+This website generates passwords using Javascript. It uses a base of HTML to layout a structure and CSS to style the structure. The Javascript first checks when the "Generate Password" button is pressed, when pressed it will execute the Generate password function, first it will clear the password, then through a series of alerts it will generate a variable num for password length and booleans for what the password should contain. The code generates first an array that contains one each character type, it will concatenate this to the rest of the password that generated throught the use of random numbers and arrays containing symbols and letters.
+
+At first I found myself creating too much code for this project, I was rewriting the random number generator multiple times, and before concatenating two arrays I had made a long series of if statements and for loops that would check and return booleans if the password contained all the required character types. I couldn't get that working and used the two arrays as a simpler solution. If I were to redo this project, I'd like to get the password checking system to work.
+
+## Table of Contents
+## How to Install and Run
+
+Table of Contents/Install and Run are here so I get used to putting them in even though I don't need them for this project
+
+## How to use
+
+If you want to generate a password press the Button labeled Generate Password
+After the pressing the button follow the series of prompts to generate an acceptable password.
